0dcb131545fe7266dd025aa95499b24638c6d90a
mosen <mosen1@gmail.com>
2016-11-29 13:12:12 +1100

    Remove jss cli target Expand methods on DistributionPointStore

--- jssapi/stores/DistributionPointStore.swift
+++ jssapi/stores/DistributionPointStore.swift
@@ -1,7 +1,32 @@
 import Foundation
 
 class DistributionPointStore : JSSStore {
+
+    /**
+      Get a list of all distribution points.
+    */
+    func all(completionHandler: @escaping ([DistributionPoint]?, Error?) -> Void) {
+        let url = self.api.url.appendingPathComponent("/JSSResource/distributionpoints")
+        let request = URLRequest(url: url)
+        
+        self.api.fetchXML(request: request) {
+            (data, response, error) in
+            
+            if let content = data {
+                let parser = XMLParser(data: content)
+                var dps: [DistributionPoint] = []
+
+                
+                return completionHandler(dps, nil)
+            } else {
+                return completionHandler(nil, StoreError.EmptyContent)
+            }
+        }
+    }
     
+    /**
+      Find a distribution point by its ID.
+    */
     func find(id: Int, completionHandler: @escaping (DistributionPoint?, Error?) -> Void) {
         let url = self.api.url.appendingPathComponent("/JSSResource/distributionpoints/id/\(id)")
         let request = URLRequest(url: url)
@@ -22,8 +47,39 @@ class DistributionPointStore : JSSStore {
             }
         }
     }
+
+    /**
+      Find a distribution point by name.
+    */
+    func find(name: String, completionHandler: @escaping (DistributionPoint?, Error?) -> Void) {
+        let url = self.api.url.appendingPathComponent("/JSSResource/distributionpoints/name/\(name)")
+        let request = URLRequest(url: url)
+        
+        self.api.fetchXML(request: request) {
+            (data, response, error) in
+            
+            if let content = data {
+                let parser = XMLParser(data: content)
+                let dp = DistributionPoint()
+                let mirrorParser = MirrorParser(reflecting: dp)
+                parser.delegate = mirrorParser
+                parser.parse()
+                
+                return completionHandler(dp, nil)
+            } else {
+                return completionHandler(nil, StoreError.EmptyContent)
+            }
+        }
+    }
     
-    func create(_ distributionPoint: DistributionPoint, completionHandler: @escaping(DistributionPoint?, Error?) -> Void) {
+    /**
+      Create a distribution point.
+
+      - parameter distributionPoint: The details of the distribution point to create. Not validated.
+      - parameter completionHandler: A completion handler that will be called with the ID of the created distribution
+      point, or an error if it was not created.
+    */
+    func create(_ distributionPoint: DistributionPoint, completionHandler: @escaping(Int?, Error?) -> Void) {
         let url = self.api.url.appendingPathComponent("/JSSResource/distributionpoints/id/0")
         var request = URLRequest(url: url)
         request.httpMethod = "POST"
@@ -37,11 +93,35 @@ class DistributionPointStore : JSSStore {
                 completionHandler(nil, err)
             }
             
-            if let r = response as? HTTPURLResponse {
-                print(r.statusCode)
+            if let body = data {
+                do {
+                    let xmlDoc = XMLDocument(data)
+                    let packageIdEl = try xmlDoc.rootElement()?.nodes(forXPath: "/package/id")
+
+                    if packageIdEl.count == 0 {
+                        return completionHandler(nil, StoreError.EmptyContent)
+                    }
+
+                    if let packageIdStr = packageIdEl[0].stringValue {
+                        let packageId = Int(packageIdStr, radix: 0)
+                        return completionHandler(packageId, nil)
+                    }
+
+
+                } catch {
+                    return completionHandler(nil, error)
+                }
             }
             
             return completionHandler(nil, StoreError.EmptyContent)
         }
+    }
+    
+    func save(_ distributionPoint: DistributionPoint, completionHandler: @escaping(Bool, Error?) -> Void) {
+        
+    }
+    
+    func delete(_ distributionPoint: DistributionPoint, completionHandler: @escaping(Bool, Error?) -> Void) {
+        
     }
 }
