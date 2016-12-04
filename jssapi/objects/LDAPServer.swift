12bcf5e2e464079be71a93035eba4a32debcf689
Mosen <mosen1@gmail.com>
2016-12-05 10:11:27 +1100

    Add NSCoding compliance to LDAPConnection

--- jssapi/objects/LDAPServer.swift
+++ jssapi/objects/LDAPServer.swift
@@ -14,7 +14,7 @@ class LDAPAccount: NSCoding {
     }
 }
 
-class LDAPConnection {
+class LDAPConnection: NSCoding {
     
     enum ServerType: String {
         case ActiveDirectory = "Active Directory"
@@ -43,6 +43,25 @@ class LDAPConnection {
     // referral_response
     var useWildcards: Bool = true
     var connectionIsUsedFor: String? = "users"
+    
+    required init?(coder aDecoder: NSCoder) {
+        
+    }
+    
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(self.id, forKey: "id")
+        aCoder.encode(self.name, forKey: "name")
+        aCoder.encode(self.hostname, forKey: "hostname")
+        aCoder.encode(self.serverType, forKey: "server_type")
+        aCoder.encode(self.port, forKey: "port")
+        aCoder.encode(self.useSsl, forKey: "use_ssl")
+        aCoder.encode(self.authenticationType, forKey: "authentication_type")
+        aCoder.encode(self.account, forKey: "account")
+        aCoder.encode(self.openCloseTimeout, forKey: "open_close_timeout")
+        aCoder.encode(self.searchTimeout, forKey: "search_timeout")
+        aCoder.encode(self.useWildcards, forKey: "use_wildcards")
+        aCoder.encode(self.connectionIsUsedFor, forKey: "connection_is_used_for")
+    }
 }
 
 class LDAPServer: JSSResource {
